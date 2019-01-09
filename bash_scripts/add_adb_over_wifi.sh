a531b46f8821aa4ba6f3d8cde8e5a21506a175ea
ayusuke7 <alexhcb.7392@gmail.com>
2019-01-09 08:42:54 -0300

    Verification not devices connect in input default port

--- bash_scripts/add_adb_over_wifi.sh
+++ bash_scripts/add_adb_over_wifi.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-echo "starting the process..."
-echo "type your device ip:"
-read ip
-eval "adb tcpip 5556"
+COMMAND=$(eval "adb devices")
 
-eval "adb connect $ip"
+if [ "$COMMAND" == "List of devices attached" ]
+then
+	echo "no connected devices"
+else	
+	echo "starting the process adb wirelless..."
+	echo "type your device ip:"
+	read IP	
+	
+	echo "type your port listen: (Enter for default 5555)"
+	read PORT
 
-echo "congratz!!!!! you can remove the usb cable now and be happy"
+	if [ -z "$PORT" ]
+	then
+		eval "adb tcpip 5555"
+	else
+		eval "adb tcpip $PORT"
+	fi	
+	
+	EXIT=$(eval "adb connect $IP")
+
+	echo "congratz!!!!! you can remove the usb cable now and be happy"
+fi
+
+
 
 
